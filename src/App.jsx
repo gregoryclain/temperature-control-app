370cc088f908c67bb292f49af4acf4cf116988f9
Grégory Clain <g.clain69@gmail.com>
2022-05-17 23:53:03 +0200

    use function to set temperature

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 
 const App = () => {
   const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    const newTemperature = temperatureValue +1;
+    setTemperatureValue(newTemperature)
+  }
+  const decreaseTemperature = () => {
+    const newTemperature = temperatureValue -1;
+    setTemperatureValue(newTemperature)
+  }
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperatureValue}°C</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => increaseTemperature() }>+</button>
+        <button onClick={() => decreaseTemperature() }>-</button>
       </div>
     </div>
   )
